091027fdc467683e390b1c84cb713889981eda34
Ollikehy <eragah@gmail.com>
2020-01-20 13:55:42 +0200

    added base for backend

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## JAFA or Just another fitness app
 
-The purpose of this application is to easily keep track of exercising.
+The purpose of this application is to easily keep track of your workout while exercising and to follow your progress.
 
 [Documentation](https://github.com/ollikehy/jafa/blob/master/documentation/rootdocument.md)
 
---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const http = require('http')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const PORT = process.env.port || 8000
+const HOST = '0.0.0.0'
+
+const app = express()
+
+app.use(morgan('dev'))
+
+const server = http.createServer(app)
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-st9gc.gcp.mongodb.net/test?retryWrites=true&w=majority`
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    console.log('connected to mongodb')
+  })
+  .catch((error) => {
+    console.log('mongodb error ' + error)
+})
+
+server.listen(PORT, () => {
+  console.log(`Server running on port http://${HOST}:${PORT}`)
+})
+
+module.exports = {app, server}
