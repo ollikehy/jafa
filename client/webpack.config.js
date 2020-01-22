c62c343ba38bc63701048e9617eaa8804df370df
Ollikehy <eragah@gmail.com>
2020-01-22 14:02:15 +0200

    configuring webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+const config = (env, arg) => {
+  return {
+    entry: ['@babel/polyfill', './src/index.js'],
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'main.js'
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'build'),
+      compress: true,
+      port: 3000,
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+        {
+          test: /\.css$/,
+          loaders: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+  }
+}
+
+module.exports = config
--- documentation/hours.md
+++ documentation/hours.md
@@ -1,6 +1,8 @@
-| Day        | Hours           | Description  |
-| ------------- |:-------------:| -----:|
+| Day             | Hours         | Description  |
+| :-------------: |:-------------:| :-----------:|
 | 20.1.2020 | 2h | Initializing repository and doing preliminary architectural planning |
 |  | 1h | Setting up backend and mongodb |
 |  | 1h | Learning some bacon.js, improving documentation (user stories and requirements) |
-| Sum | 4h | Cumulative sum of hours |
+| 21.1.2020 | 1h | Literally fighting with formatting for an hour |
+| 22.2.2020 | 1,5h | Configuring webpack |
+| Sum | 5h | Cumulative sum of hours |
