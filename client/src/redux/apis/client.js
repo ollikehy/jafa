03c8bb58f4bee8b4cd61e5a9c60b1b7a4ca073fd
Olli Keski-Hynnilä <eragah@gmail.com>
2020-12-07 12:55:00 +0200

    fixed lint issues

--- client/src/redux/apis/client.js
+++ client/src/redux/apis/client.js
@@ -1,8 +1,9 @@
-const axios = require('axios')
+const axios = import('axios')
 
+// eslint-disable-next-line
 const url = process.env.NODE_ENV === 'production' ? 'https://jafa-backend.herokuapp.com/' : '/api'
 const axiosClient = axios.create({
-    baseURL: url
+  baseURL: url
 })
 
 export default axiosClient
