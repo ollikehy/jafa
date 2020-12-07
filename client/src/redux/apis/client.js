1c00684eab6c816366dab949f2069b5fed01c501
Olli Keski-Hynnilä <eragah@gmail.com>
2020-12-07 13:29:02 +0200

    hopefully fixed issue with axios and tried to fix a test

--- client/src/redux/apis/client.js
+++ client/src/redux/apis/client.js
@@ -1,6 +1,6 @@
-const axios = import('axios')
+/*eslint-disable*/
+const axios = require('axios')
 
-// eslint-disable-next-line
 const url = process.env.NODE_ENV === 'production' ? 'https://jafa-backend.herokuapp.com/' : '/api'
 const axiosClient = axios.create({
   baseURL: url
