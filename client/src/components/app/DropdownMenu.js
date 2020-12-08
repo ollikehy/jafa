0f8a98e73e976f332a307ae31ec14c1723dc571b
Olli Keski-Hynnilä <eragah@gmail.com>
2020-12-08 13:39:44 +0200

    fonts and hamburger menu stuff

--- client/src/components/app/DropdownMenu.js
+++ client/src/components/app/DropdownMenu.js
@@ -1,5 +1,12 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 import enhanceWithClickOutside from 'react-click-outside'
+
+import Hamburger from '../../assets/images/hamburger.png'
+
+import * as actions from '../../redux/actions/actions'
 
 export class DropdownMenu extends Component {
   constructor(props) {
@@ -11,7 +18,9 @@ export class DropdownMenu extends Component {
   }
 
   handleClickOutside = () => {
-    this.toggleList()
+    this.setState({
+      listOpen: false
+    })
   }
 
   toggleList = () => {
@@ -21,19 +30,46 @@ export class DropdownMenu extends Component {
   }
 
   render() {
-    const {listOpen} = this.state
+    const { listOpen } = this.state
+    const { loggedIn, logout } = this.props
 
     return (
-      <div className='dropdown-wrapper'>
+      <div>
         <div className='dropdown-header' onClick={this.toggleList}>
-          Exercises
+          <img style={{ width: 40, height: 40 }} src={Hamburger}></img>
         </div>
-        {listOpen && <div className='dropdown-content'>
-          <p className='dropdown-item'>Dropdown item</p>
-        </div>}
+        {listOpen && (
+          !loggedIn ? <div className='dropdown-content'>
+            <Link className='dropdown-item' to='/exercise'>Exercises</Link>
+            <Link className='dropdown-item' to='/login'>Login</Link>
+            <Link className='dropdown-item' to='/register'>Register</Link>
+          </div>
+            : <div className='dropdown-content'>
+              <Link className='dropdown-item' to='/workout'>Workouts</Link>
+              <Link className='dropdown-item' to='/exercise'>Exercises</Link>
+              <Link className='dropdown-item' to='/profile'>Profile</Link>
+              <div className='dropdown-item' onClick={logout}>Logout</div>
+            </div>)
+        }
       </div>
     )
   }
 }
 
-export default enhanceWithClickOutside(DropdownMenu)
+DropdownMenu.propTypes = {
+  loggedIn: PropTypes.object,
+  logout: PropTypes.func
+}
+
+const mapDispatchToProps = {
+  ...actions
+}
+
+const mapStateToProps = (state) => ({
+  loggedIn: state.loginReducer.loggedIn,
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps)
+  (enhanceWithClickOutside(DropdownMenu))
--- client/src/components/app/Header.js
+++ client/src/components/app/Header.js
@@ -1,16 +1,37 @@
-import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
-import {connect} from 'react-redux'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import DropdownMenu from './DropdownMenu'
 
 import JafaIcon from '../../assets/images/jafa-icon.png'
 
 import * as actions from '../../redux/actions/actions'
 
 export class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isDesktop: false
+    }
+    this.updateState = this.updateState.bind(this)
+  }
+
+  componentDidMount() {
+    this.updateState()
+    window.addEventListener('resize', this.updateState)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateState)
+  }
+
+  updateState() {
+    this.setState({ isDesktop: window.innerWidth > 1000 })
+  }
 
   render() {
-    const {loggedIn, logoutError, logout} = this.props
+    const { loggedIn, logoutError, logout } = this.props
 
     return (
       <div className='header'>
@@ -20,17 +41,19 @@ export class Header extends Component {
           </Link>
         </div>
         {logoutError && window.alert(logoutError)}
-        {!loggedIn && <div className='header-menu'>
-          <Link className='header-link' to='/exercise'>Exercises</Link>
-          <Link className='header-link' to='/login'>Login</Link>
-          <Link className='header-link' to='/register'>Register</Link>
-        </div>}
-        {loggedIn && <div className='header-menu'>
-          <Link className='header-link' to='/workout'>Workouts</Link>
-          <Link className='header-link' to='/exercise'>Exercises</Link>
-          <Link className='header-link' to='/profile'>Profile</Link>
-          <button className='header-logout' onClick={logout}>Logout</button>
-        </div>}
+        {this.state.isDesktop ?
+          !loggedIn ? <div className='header-menu'>
+            <Link className='header-link' to='/exercise'>Exercises</Link>
+            <Link className='header-link' to='/login'>Login</Link>
+            <Link className='header-link' to='/register'>Register</Link>
+          </div>
+            : <div className='header-menu'>
+              <Link className='header-link' to='/workout'>Workouts</Link>
+              <Link className='header-link' to='/exercise'>Exercises</Link>
+              <Link className='header-link' to='/profile'>Profile</Link>
+              <div className='header-logout' onClick={logout}>Logout</div>
+            </div>
+          : <DropdownMenu />}
       </div>
     )
   }
